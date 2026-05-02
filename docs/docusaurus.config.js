f15c5af5d8fbc3cebaddd90f40c018967f3c2288
Sukhorosov Aleksey <39103632+alexdrydew@users.noreply.github.com>
2026-05-03 00:35:25 +0200

    configure docusaurus (#3)

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,20 @@
+const config = {
+  title: 'Inlay',
+  tagline: 'Static, structural dependency injection for Python',
+  url: 'https://alexdrydew.github.io',
+  baseUrl: '/inlay/',
+  organizationName: 'alexdrydew',
+  projectName: 'inlay',
+  trailingSlash: false,
+  presets: [
+    [
+      'classic',
+      {
+        docs: false,
+        blog: false,
+      },
+    ],
+  ],
+};
+
+module.exports = config;
